778471a3389aa0db95db7fec9861ec2b67b60362
Virx <virx@virxcase.dev>
2025-02-28 18:29:44 -0500

    Fix error

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import { MAPS_STANDARD } from './arena-names';
 
-export const mapStore = writable(localStorage.getItem("MS_MAP") || MAPS_STANDARD.DFHStadium);
+export const mapStore = writable(localStorage.getItem("MS_MAP") || MAPS_STANDARD["DFH Stadium"]);
 mapStore.subscribe(value => {
     localStorage.setItem("MS_MAP", value);
 });
